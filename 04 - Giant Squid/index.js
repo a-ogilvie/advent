6463da535df0352bb7e2af59917c095f0faf0a3c
a-ogilvie <alexander.ogilvie@icloud.com>
2021-12-04 20:43:06 +0900

    Day 4

---
+++ 04 - Giant Squid/index.js
@@ -0,0 +1,76 @@
+/* eslint-disable no-magic-numbers */
+'use strict';
+
+const _ = require('lodash');
+
+/** @typedef {Array<number | null>} Board */
+
+/**
+ * @param {{numbers: Array<number>, boards: Array<Board>}} input
+ * @returns {number}
+ */
+function part1({ numbers, boards }) {
+  for (const number of numbers)
+    for (const board of boards) {
+      const indexOfNumber = board.indexOf(number);
+      if (indexOfNumber === -1) continue;
+
+      board[indexOfNumber] = null;
+
+      if (isBingo(board)) return _.sum(board) * number;
+    }
+}
+
+/**
+ * @param {{numbers: Array<number>, boards: Array<Board>}} input
+ * @returns {number}
+ */
+function part2({ numbers, boards }) {
+  for (const number of numbers)
+    for (let i = 0; i < boards.length; i += 1) {
+      const board = boards[i];
+
+      const indexOfNumber = board.indexOf(number);
+      if (indexOfNumber === -1) continue;
+
+      board[indexOfNumber] = null;
+
+      if (isBingo(board)) {
+        if (boards.length === 1) return _.sum(board) * number;
+
+        const indexToRemove = boards.indexOf(board);
+        boards.splice(indexToRemove, 1);
+        i -= 1;
+      }
+    }
+}
+
+/**
+ * @param {Board} board
+ * @returns {boolean}
+ */
+function isBingo(board) {
+  for (const i of [0, 1, 2, 3, 4]) {
+    if (
+      board[i] === null &&
+      board[i + 5] === null &&
+      board[i + 10] === null &&
+      board[i + 15] === null &&
+      board[i + 20] === null
+    )
+      return true;
+
+    if (
+      board[i * 5] === null &&
+      board[i * 5 + 1] === null &&
+      board[i * 5 + 2] === null &&
+      board[i * 5 + 3] === null &&
+      board[i * 5 + 4] === null
+    )
+      return true;
+  }
+
+  return false;
+}
+
+module.exports = { part1, part2 };
---
+++ 04 - Giant Squid/index.test.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-magic-numbers */
+'use strict';
+
+const { expect } = require('chai');
+
+const puzzleInput = require('./input');
+
+const { part1, part2 } = require('./index');
+
+describe('Day 04: Giant Squid', () => {
+  const testInput = {
+    numbers: [
+      7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26,
+      1,
+    ],
+
+    boards: [
+      [22, 13, 17, 11, 0, 8, 2, 23, 4, 24, 21, 9, 14, 16, 7, 6, 10, 3, 18, 5, 1, 12, 20, 15, 19],
+      [3, 15, 0, 2, 22, 9, 18, 13, 17, 5, 19, 8, 7, 25, 23, 20, 11, 10, 24, 4, 14, 21, 16, 12, 6],
+      [14, 21, 17, 24, 4, 10, 16, 15, 9, 19, 18, 8, 23, 26, 20, 22, 11, 13, 6, 5, 2, 0, 12, 3, 7],
+    ],
+  };
+
+  describe('Part 1', () => {
+    it('should calculate the score of the board that wins first', () => {
+      expect(part1(testInput)).to.equal(4512);
+
+      console.log(part1(puzzleInput));
+    });
+  });
+
+  describe('Part 2', () => {
+    it('should calculate the score of the board that wins last', () => {
+      expect(part2(testInput)).to.equal(1924);
+
+      console.log(part2(puzzleInput));
+    });
+  });
+});
