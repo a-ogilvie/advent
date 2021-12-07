8c1be46dc43d86721a8732525fd9ec692203753e
Alexander Ogilvie <aogilvie@indeed.com>
2021-12-07 15:04:03 +0900

    refactor: Day 01

--- 01 - Sonar Sweep/index.js
+++ 01 - Sonar Sweep/index.js
@@ -1,17 +1,33 @@
 'use strict';
 
 /**
- * @param {Array<number>} values
- * @param {number} [windowSize]
+ * @param {Array<number>} input
  * @returns {number}
  */
-function partOne(values, windowSize = 1) {
+function partOne(input) {
+  return getIncreasingCount(input, 1);
+}
+
+/**
+ * @param {Array<number>} input
+ * @returns {number}
+ */
+function partTwo(input) {
+  return getIncreasingCount(input, 3); // eslint-disable-line no-magic-numbers
+}
+
+/**
+ * @param {Array<number>} input
+ * @param {number} windowSize
+ * @returns {number}
+ */
+function getIncreasingCount(input, windowSize) {
   let increasingCount = 0;
 
-  for (let i = windowSize; i < values.length; i += 1)
-    if (values[i] > values[i - windowSize]) increasingCount += 1;
+  for (let i = windowSize; i < input.length; i += 1)
+    if (input[i] > input[i - windowSize]) increasingCount += 1;
 
   return increasingCount;
 }
 
-module.exports = { partOne };
+module.exports = { partOne, partTwo };
--- 01 - Sonar Sweep/index.test.js
+++ 01 - Sonar Sweep/index.test.js
@@ -5,7 +5,7 @@ const { expect } = require('chai');
 
 const puzzleInput = require('./input');
 
-const { partOne } = require('./index');
+const { partOne, partTwo } = require('./index');
 
 describe('Day 01: Sonar Sweep', () => {
   const testInput = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
@@ -20,9 +20,9 @@ describe('Day 01: Sonar Sweep', () => {
 
   describe('Part 2', () => {
     it('should use a three-measurement window to count the number of values with increasing depth', () => {
-      expect(partOne(testInput, 3)).to.equal(5);
+      expect(partTwo(testInput)).to.equal(5);
 
-      console.log(partOne(puzzleInput, 3));
+      console.log(partTwo(puzzleInput));
     });
   });
 });
