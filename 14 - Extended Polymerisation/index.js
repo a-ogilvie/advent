2c26c2f4005f009450799335a09b6e1dcd4f28ef
Alexander Ogilvie <aogilvie@indeed.com>
2021-12-14 15:59:21 +0900

    Day 14

---
+++ 14 - Extended Polymerisation/index.js
@@ -0,0 +1,68 @@
+/* eslint-disable no-magic-numbers */
+'use strict';
+
+/**
+ * @param {{ pairInsertionRules:  Map<string, string>, polymerTemplate: string }} input
+ * @returns {number}
+ */
+function partOne({ pairInsertionRules, polymerTemplate }) {
+  return calculateMostCommonLeastCommonElementDifference(pairInsertionRules, polymerTemplate, 10);
+}
+
+/**
+ * @param {{ pairInsertionRules:  Map<string, string>, polymerTemplate: string }} input
+ * @returns {number}
+ */
+function partTwo({ pairInsertionRules, polymerTemplate }) {
+  return calculateMostCommonLeastCommonElementDifference(pairInsertionRules, polymerTemplate, 40);
+}
+
+/**
+ * @param {Map<string, string>} pairInsertionRules
+ * @param {string} polymerTemplate
+ * @param {number} steps
+ * @returns {number}
+ */
+function calculateMostCommonLeastCommonElementDifference(
+  pairInsertionRules,
+  polymerTemplate,
+  steps
+) {
+  const pairCountByPair = Object.fromEntries(
+    [...pairInsertionRules.keys()].map((pair) => [pair, 0])
+  );
+
+  for (let i = 0; i < polymerTemplate.length - 1; i += 1) {
+    const pair = polymerTemplate[i] + polymerTemplate[i + 1];
+    pairCountByPair[pair] += 1;
+  }
+
+  for (let step = 1; step <= steps; step += 1) {
+    const pairs = Object.entries(pairCountByPair);
+
+    for (const [pair, count] of pairs) {
+      const elementInsert = pairInsertionRules.get(pair);
+      pairCountByPair[pair] -= count;
+
+      const [elementA, elementB] = pair.split('');
+      pairCountByPair[elementA + elementInsert] += count;
+      pairCountByPair[elementInsert + elementB] += count;
+    }
+  }
+
+  const elementCountByElement = Object.fromEntries(
+    [...pairInsertionRules.values()].map((element) => [element, 0])
+  );
+
+  for (const [[elementA], count] of Object.entries(pairCountByPair))
+    elementCountByElement[elementA] += count;
+
+  const finalElement = polymerTemplate[polymerTemplate.length - 1];
+  elementCountByElement[finalElement] += 1;
+
+  const elementCounts = Object.values(elementCountByElement);
+
+  return Math.max(...elementCounts) - Math.min(...elementCounts);
+}
+
+module.exports = { partOne, partTwo };
---
+++ 14 - Extended Polymerisation/index.test.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-magic-numbers */
+'use strict';
+
+const { expect } = require('chai');
+
+const puzzleInput = require('./input');
+
+const { partOne, partTwo } = require('./index');
+
+describe('Day 14: Extended Polymerisation', () => {
+  /** @type {{ pairInsertionRules: Map<string, string>, polymerTemplate: string }} */
+  const testInput = {
+    pairInsertionRules: new Map([
+      ['CH', 'B'],
+      ['HH', 'N'],
+      ['CB', 'H'],
+      ['NH', 'C'],
+      ['HB', 'C'],
+      ['HC', 'B'],
+      ['HN', 'C'],
+      ['NN', 'C'],
+      ['BH', 'H'],
+      ['NC', 'B'],
+      ['NB', 'B'],
+      ['BN', 'B'],
+      ['BB', 'N'],
+      ['BC', 'B'],
+      ['CC', 'N'],
+      ['CN', 'C'],
+    ]),
+    polymerTemplate: 'NNCB',
+  };
+
+  describe('Part 1', () => {
+    it('should return the difference between the most common and least common chars after 10 steps of polymerisation', () => {
+      expect(partOne(testInput)).to.equal(1588);
+
+      console.log(partOne(puzzleInput));
+    });
+  });
+
+  describe('Part 2', () => {
+    it('should return the difference between the most common and least common chars after 40 steps of polymerisation', () => {
+      expect(partTwo(testInput)).to.equal(2188189693529);
+
+      console.log(partTwo(puzzleInput));
+    });
+  });
+});
